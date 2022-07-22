5bcdd24642ea5003d883a5635d457ea0e524958f
andrii maliuta <quadr988"gmail.com>
2022-07-22 21:02:55 +0300

    init

--- src/Main.purs
+++ src/Main.purs
@@ -1,5 +1,26 @@
--- module Example where
+module Example where
 
--- import Effect.Console (log)
+import Effect.Console (log)
+
+import Prelude
+
+import Control.Plus (empty)
+import Data.Array ((..))
 
 -- main = log "Hello, World!"
+
+countThrows :: Int -> Array (Array Int)
+countThrows n = do
+  x <- 1 .. 6
+  y <- 1 .. 6
+  if x + y == n
+    then pure [ x, y ]
+    else empty
+
+userCity :: XML -> Maybe XML
+userCity root =
+  child root "profile" >>= \prof ->
+    child prof "address" >>= \addr ->
+      child addr "city" >>= \city ->
+        pure city
+
--- src/fs.purs
+++ src/fs.purs
@@ -17,3 +17,19 @@ allFiles' :: Path -> Array Path
 allFiles' file = file : do
   child <- ls file
   allFiles' child
+
+-- Using ADTs
+
+showShape :: Shape -> String
+showShape (Circle c r) =
+  "Circle [center: " <> showPoint c <> ", radius: " <> show r <> "]"
+showShape (Rectangle c w h) =
+  "Rectangle [center: " <> showPoint c <> ", width: " <> show w <> ", height: " <> show h <> "]"
+showShape (Line start end) =
+  "Line [start: " <> showPoint start <> ", end: " <> showPoint end <> "]"
+showShape (Text loc text) =
+  "Text [location: " <> showPoint loc <> ", text: " <> show text <> "]"
+
+showPoint :: Point -> String
+showPoint { x, y } =
+  "(" <> show x <> ", " <> show y <> ")"
